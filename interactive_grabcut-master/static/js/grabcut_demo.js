a020cdd2816d6154a8ed0ce692d85420a22ffc02
Kanishk Chathley <kchathley26@gmail.com>
2022-06-30 01:33:48 +0530

    dynamic image

--- interactive_grabcut-master/static/js/grabcut_demo.js
+++ interactive_grabcut-master/static/js/grabcut_demo.js
@@ -1,16 +1,42 @@
-$(document).ready(function(){
-    console.log('doc ready')
-    all_rows = {} // id: objc
-    appending_container = "appending_container"
+// var uploaded_image;
+$(document).ready(function () {
+  console.log("doc ready");
+});
 
-    project_name_1 = "burger"
-    let obj1 = makeGrabCutRow(appending_container, "../static/images/burger/_original/burger.jpg", project_name_1)
-    let obj1_id = obj1.id
-    all_rows[obj1_id] = obj1
+var loadFile = function (event) {
+  console.log("change");
+  uploaded_image = URL.createObjectURL(event.target.files[0]);
+};
 
-    //project_name_2 = "chess"
-    //let obj2 = makeGrabCutRow(appending_container, "../static/images/chess/_original/test_chess.jpeg", project_name_2)
-    //let obj2_id = obj2.id
-    //all_rows[obj2_id] = obj2
+const startButton = document.getElementById("start-button");
+var runApp = function () {
+  console.log("running");
+  all_rows = {}; // id: objc
+  appending_container = "appending_container";
 
-})
+  project_name_1 = "burger";
+  let obj1 = makeGrabCutRow(
+    appending_container,
+    uploaded_image,
+    project_name_1
+  );
+  let obj1_id = obj1.id;
+  all_rows[obj1_id] = obj1;
+}
+//   all_rows = {}; // id: objc
+//   appending_container = "appending_container";
+
+//   project_name_1 = "burger";
+//   let obj1 = makeGrabCutRow(
+//     appending_container,
+//     `url(${uploaded_image})`,
+//     project_name_1
+//   );
+//   let obj1_id = obj1.id;
+//   all_rows[obj1_id] = obj1;
+
+//project_name_2 = "chess"
+//let obj2 = makeGrabCutRow(appending_container, "../static/images/chess/_original/test_chess.jpeg", project_name_2)
+//let obj2_id = obj2.id
+//all_rows[obj2_id] = obj2
+// });
